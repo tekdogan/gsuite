eeb350284d38c9e584c9a73e4a78ab4452a9ff1d
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2021-12-20 14:32:11 +0300

    remove GCN layer from MP::SAG

--- cuda/CU_SAG_MP.cu
+++ cuda/CU_SAG_MP.cu
@@ -13,26 +13,6 @@
 
 namespace CU_MP {
 
-void GCNLayer(float* edgeIndex, float* featureTensor, float* aggregationVar, float* nodeDegrees) {
-/*
-        for(int i=0; i<NUM_NODES; i++) {
-                for(int j=0; j<DIRECTED_EDGES; j++) {
-                        if(edgeIndex[0][j] == i) {// if there is an edge incoming to node i
-                                // aggregate edgeIndex[1][j] features on node i
-                                //std::cout << "from node " << edgeIndex[1][j] << " to node " << i << std::endl;
-                                for(int k=0; k<FEATURE_LEN; k++) {
-                                        aggregationVar[k] += featureTensor[i][k] * 1.0/sqrt(nodeDegrees[i]*nodeDegrees[(int)edgeIndex[1][j]]);
-                                }
-                        }
-                }
-                featureTensor[i][0] = aggregationVar[0];
-                featureTensor[i][1] = aggregationVar[1];
-                aggregationVar[0] = 0.0;
-                aggregationVar[1] = 0.0;
-        }
-*/
-}
-
 __global__ void SAGLayer(float* edgeIndex, float* featureTensor, float *aggregationVar, float *nodeDegrees, float w1, float w2) {
 
 	int i = threadIdx.x;
