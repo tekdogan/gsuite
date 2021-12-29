f19d0f002cdf040d0786e800d7b474d5b1055793
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2021-12-29 13:38:30 +0300

    Update CU_GCN_MP.cu

--- cuda/CU_GCN_MP.cu
+++ cuda/CU_GCN_MP.cu
@@ -13,7 +13,8 @@
 
 namespace CU_MP {
 
-void GCNLayer(float* edgeIndex, float* featureTensor, float* aggregationVar, float* nodeDegrees) {
+void GCNLayer(float* edgeIndex, float* featureTensor, float *aggregationVar, float *nodeDegrees,
+	      	int numOfNodes, int numOfFeatures, int numOfEdges) {
 
 	int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
 	
@@ -33,28 +34,10 @@ void GCNLayer(float* edgeIndex, float* featureTensor, float* aggregationVar, flo
 			  + id_exFeatures) += *(featureTensor + thread_idx) *
 				1.0/sqrt(nodeDegrees[id_exNodes]*
 					 nodeDegrees[( (int)*(edgeIndex + numOfDirectedEdges + id_exEdges) )]);
-			
 		}
 		
 	}
 	
-/*
-        for(int i=0; i<NUM_NODES; i++) {
-                for(int j=0; j<DIRECTED_EDGES; j++) {
-                        if(edgeIndex[0][j] == i) {// if there is an edge incoming to node i
-                                // aggregate edgeIndex[1][j] features on node i
-                                //std::cout << "from node " << edgeIndex[1][j] << " to node " << i << std::endl;
-                                for(int k=0; k<FEATURE_LEN; k++) {
-                                        aggregationVar[k] += featureTensor[i][k] * 1.0/sqrt(nodeDegrees[i]*nodeDegrees[(int)edgeIndex[1][j]]);
-                                }
-                        }
-                }
-                featureTensor[i][0] = aggregationVar[0];
-                featureTensor[i][1] = aggregationVar[1];
-                aggregationVar[0] = 0.0;
-                aggregationVar[1] = 0.0;
-        }
-*/
 }
 
 __global__ void GCNLayerNew(float* edgeIndex, float* featureTensor, float *aggregationVar, float *nodeDegrees, int numOfNodes, int numOfFeatures, int numOfEdges) {
