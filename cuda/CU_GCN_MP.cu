b8f79f6aca6f481d434d4dcc93accf14c9b8b7b7
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2022-01-04 11:48:38 +0300

    allocs and migrates of host/device arrays

--- cuda/CU_GCN_MP.cu
+++ cuda/CU_GCN_MP.cu
@@ -9,25 +9,36 @@
 namespace CU_MP {
 
 
-void GCNLayer(float* h_edgeIndex, float* h_featureVector, float *h_aggregationVar, float *h_nodeDegrees,
+void GCNLayer(float* d_edgeIndex, float* d_featureVector, float *d_aggregationVar, float *d_nodeDegrees,
 		int numOfNodes, int numOfFeatures, int numOfEdges) {
 
-	//int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	// variable declarations for host variables
+	float *h_edgeIndex, *h_featureVector, *h_aggregationVar, *h_nodeDegrees;
 
-	float *d_edgeIndex, *d_featureVector, *d_aggregationVar, *d_nodeDegrees;
-
+	// allocations for host variables
+	*h_edgeIndex = calloc(numOfEdges * 2, sizeof(float));
+	*h_featureVector = calloc(numOfNodes * numOfFeatures, sizeof(float));
+	*h_aggregationVar = calloc(numOfNodes * numOfFeatures, sizeof(float));
+	*h_nodeDegrees = calloc(numOfNodes, sizeof(float));
+	
 	//cudaMalloc( (void**) &d_featureVector, numOfNodes*numOfFeatures * sizeof(float));
 	//cudaMemcpy(d_featureVector, h_featureVector, numOfNodes*numOfFeatures * sizeof(float), cudaMemcpyHostToDevice);
 
 	
-	// compute the node degrees
+	// compute the node degrees via scatter_add
 	auto res = scatter_cuda(h_nodeDegrees, h_edgeIndex, 1, "sum", numOfNodes, numOfFeatures, numOfEdges);
 
-	//migrate to host	
+	// migrate device degrees output to host
+	cudaMemcpy(d_nodeDegrees, h_nodeDegrees, numOfNodes * sizeof(float), cudaMemcpyHostToDevice);
+	
 	// sqrt -0.5 of node degrees
 	for(int i=0; i<numOfNodes; i++) {
-//		*(h_nodeDegrees + i) = 1/sqrt(*(h_nodeDegrees + i));
+		*(h_nodeDegrees + i) = 1/sqrt(*(h_nodeDegrees + i));
 	}
+	
+	// migrate host degrees back to device
+	cudaMemcpy(h_nodeDegrees, d_nodeDegrees, numOfNodes * sizeof(float), cudaMemcpyDeviceToHost);
 	
 	// aggregation scheme
 	//auto out = scatter_cuda(h_featureVector, h_edgeIndex, 1, "sum", numOfNodes, numOfFeatures, numOfEdges);
