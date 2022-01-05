3ee7875267b91c1dbad0fb3229daebf01e949aa8
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2022-01-05 15:43:08 +0300

    return type revised

--- cuda/scatter_cuda.h
+++ cuda/scatter_cuda.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <tuple>
 #include <iostream>
 
-std::tuple<float*, float*>
-scatter_cuda(float *src, float *index, int64_t dim,
+float* scatter_cuda(float *src, float *index, int64_t dim,
              std::string reduce, int numOfNodes, int numOfFeatures,
              int numOfEdges);
