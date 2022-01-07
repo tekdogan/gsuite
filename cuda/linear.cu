15317bd301ea576c086db47678dcdb0323e5b3b8
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2022-01-07 19:36:27 +0300

    import libraries

--- cuda/linear.cu
+++ cuda/linear.cu
@@ -1,3 +1,8 @@
+#include <cuda_runtime_api.h> // cudaMalloc, cudaMemcpy, etc.
+#include <cusparse.h>         // cusparseSpMM
+#include <cublas_v2.h>
+#include "cuBlasUtil.h"
+
 void linear(float *src, int srcRows, int srcCols
               float *out, int outRows, int outCols,
                float *bias) {
