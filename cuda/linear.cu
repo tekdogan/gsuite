8fb4d38d667e4f1178b6c1f52135293eb3be8f17
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2022-01-07 19:34:39 +0300

    Create linear.cu

---
+++ cuda/linear.cu
@@ -0,0 +1,20 @@
+void linear(float *src, int srcRows, int srcCols
+              float *out, int outRows, int outCols,
+               float *bias) {
+  
+  float *w;
+  
+  // allocate device memory for output
+  //cudaMalloc(&y,outRows*outCols*sizeof(float));
+  
+  // allocate device memory for weight
+  cudaMalloc(&w,srcCols*sizeof(float));
+  
+  // init weight matrix
+  initIdentityGPU<<<srcCols/128,128>>>(&w, srcCols, 1);
+  
+  gpu_blas_mmul(w, src, out, srcRows, srcCols, outCols, false, false, 1.0, 0.0);
+  
+  //cudaMemcpy(out,y,outRows*outCols*sizeof(float),cudaMemcpyDeviceToHost);
+  
+}
