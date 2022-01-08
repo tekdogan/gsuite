db41203f3d8e60afbc2ddeb0a2cde5f41470cca4
tekdogan <tahatekdogan97@hotmail.com>
2022-01-08 18:39:34 +0300

    test methods for indexSelect kernel

---
+++ cuda/test/main.cu
@@ -0,0 +1,51 @@
+// This function is designed for a temporary use of
+// direct usage of CUDA data loader utility from
+// a cpp based main file.
+
+//#include"DataLoader.h"
+#include "index_select.h"
+#include "Data_Util.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+	//if(argc == 1) {
+	//	std::cout << "Please pass a parameter to executable. (e.g. ./cudaDataLoader.o 2)\n";
+	//}
+
+	//LoadData(atoi(argv[1]));
+
+	float h_src[9] = {1,2,3,
+		    4,5,6,
+		    7,8,9};
+
+	int srcRows = 3;
+	int srcCols = 3;
+
+	int dim = 0;
+
+	int indices[2] = {0,2};
+
+	int indSize = 2;
+
+	int dstSize = 6;
+
+	float *h_out = (float*)calloc(dstSize, sizeof(float));
+
+	float *d_src, *d_out;
+
+	cudaMalloc((void**) &d_src, srcRows*srcCols*sizeof(float));
+	cudaMemcpy(d_src, h_src, srcRows*srcCols*sizeof(float), cudaMemcpyHostToDevice);
+
+	cudaMalloc((void**) &d_out, dstSize*sizeof(float));
+        cudaMemcpy(d_out, h_out, dstSize*sizeof(float), cudaMemcpyHostToDevice);
+
+	index_select(h_src, srcRows, srcCols,
+                    dim, indices, indSize, h_out);
+
+	cudaMemcpy(h_out, d_out, dstSize*sizeof(float), cudaMemcpyDeviceToHost);
+
+	printDenseMatrix(h_src, dstSize/srcCols, srcCols);
+
+	return 0;
+}
