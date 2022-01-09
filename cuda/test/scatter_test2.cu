cab38812fad0058035747ca1fcc0de14913b151c
tekdogan <tahatekdogan97@hotmail.com>
2022-01-09 18:47:01 +0300

    updated scatter kernel tests

---
+++ cuda/test/scatter_test2.cu
@@ -0,0 +1,52 @@
+// This function is designed for a temporary use of
+// direct usage of CUDA data loader utility from
+// a cpp based main file.
+
+//#include"DataLoader.h"
+#include "../scatter_cuda.h"
+#include "../Data_Util.h"
+#include <iostream>
+
+#include <unistd.h>
+
+/*
+
+edge_index 
+source 0,0,0,1,1,2,2,2,3,3,4,4
+dest   1,2,4,0,3,0,3,4,1,2,0,2
+*/
+
+int main(int argc, char *argv[]) {
+
+        //if(argc == 1) {
+        //      std::cout << "Please pass a parameter to executable. (e.g. ./cudaDataLoader.o 2)\n";
+        //}
+
+        //LoadData(atoi(argv[1]));
+
+	/*
+		3x4 node to feature vector
+	*/
+
+        float h_src[12] = {1};
+
+	int featureLen = 4;
+	int nodeCount = 5;
+	int edgeCount = 12;	
+
+	float *h_out = (float*)calloc(nodeCount,sizeof(float));
+
+	float h_edgeSource[12] = {0,0,0,1,1,2,2,2,3,3,4,4};
+	float h_edgeDest[12] = {1,2,4,0,3,0,3,4,1,2,0,2};
+
+
+	float* out = scatter_cuda(h_src, h_edgeSource, 1, "sum", edgeCount, 1, 12);
+
+	sleep(10);
+
+	printDenseMatrix(out, 1, nodeCount);
+
+        return 0;
+}
+
+
