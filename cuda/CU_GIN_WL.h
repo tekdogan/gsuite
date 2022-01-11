f43ffee01f6bb431d0fc88707bc893d010f6c3ee
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2022-01-11 10:24:58 +0300

    changed return type and params

--- cuda/CU_GIN_WL.h
+++ cuda/CU_GIN_WL.h
@@ -1,6 +1,6 @@
 namespace CU_WL {
 
-__global__ void GINLayer(float* edgeIndex, float* featureTensor, float *aggregationVar, float epsilon,
-                        int numOfNodes, int numOfDirectedEdges, int numOfFeatures, float* outputFeatureMatrix);
+float* GINLayer(float* edgeIndex, float* featureTensor, float *aggregationVar, float epsilon, int numOfNodes,
+	      int numOfDirectedEdges, int numOfFeatures, float* outputFeatureMatrix, int outputSize);
 
 }
