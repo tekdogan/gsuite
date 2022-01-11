021e76f6a26b9dc8f0cc4784d74088c28dfa09d2
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2022-01-11 10:24:12 +0300

    GIN layer built up with new kernels

--- cuda/CU_GIN_WL.cu
+++ cuda/CU_GIN_WL.cu
@@ -4,60 +4,77 @@
 
 namespace CU_WL {
 
-__global__ void GINLayer(float* edgeIndex, float* featureTensor, float *aggregationVar, float epsilon,
-			int numOfNodes, int numOfDirectedEdges, int numOfFeatures, float* outputFeatureMatrix) {
+float* GINLayer(float* edgeIndex, float* featureTensor, float *aggregationVar, float epsilon, int numOfNodes,
+	      int numOfDirectedEdges, int numOfFeatures, float* outputFeatureMatrix, int outputSize) {
 
-    int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    //printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d\n", blockIdx.x, blockDim.x, threadIdx.x);
-    //printf("thread_idx is: %d\n", thread_idx);
-
-    if (thread_idx < numOfNodes*numOfFeatures) {
-    
-    // get indices of the thread
-    
-    //printf("thread_idx is: %d\n", thread_idx);
-
-    //printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d\n", blockIdx.x, blockDim.x, threadIdx.x);
-
-    const int64_t id_exEdges = (thread_idx % numOfDirectedEdges);
-
-    const int64_t id_exNodes = (thread_idx / numOfFeatures);
-
-    const int64_t id_exFeatures = (thread_idx / numOfNodes);
-
-    // if an incoming edge to respected node
-    if( *(edgeIndex + numOfDirectedEdges + id_exEdges) == id_exNodes ) {
-	// then apply aggregation scheme of GCN
-	// to corresponding node's feature
-	*(aggregationVar + (int)numOfFeatures*( (int)*(edgeIndex + numOfDirectedEdges + id_exEdges) )
-	    + id_exFeatures) = *(featureTensor + thread_idx);
-    }
+	// first part of edgeIndex indicating sources
+	int *edgeSources = (int*)calloc(numOfEdges, sizeof(int));
+	//memcpy(edgeSources, edgeIndex, numOfEdges*sizeof(int));
+	for(int i = 0; i<numOfEdges; i++)
+	{
+		edgeSources[i] = edgeIndex[i];
+	}
 	
-    //sync threads before output update
-    //__syncthreads();
-
-    // update output feature values
-	*(outputFeatureMatrix + thread_idx) =
-//    *(outputFeatureMatrix + numOfFeatures*id_exNodes + id_exFeatures) =
-	    (1 + epsilon)*(*(outputFeatureMatrix + thread_idx)) +
-	    *(aggregationVar + thread_idx);
-
-    }
-
-    // the below part is on hold due to kernel update
-    //if(i < numOfNodes) {
-                //for(int j=0; j<numOfDirectedEdges; j++) {
-			//if((*(edgeIndex + j)) == (float)i) { // if there is an edge incoming to node i
-				//for(int k=0; k<numOfFeatures; k++) {
-					//*(aggregationVar + i*numOfFeatures + k) += *(featureTensor + i*numOfFeatures + k);
-				//}
-			//}
-                //}
-
-		//for(int k=0; k<numOfFeatures; k++) {
-			//*(outputFeatureMatrix + i*numOfFeatures + k) = (1 + epsilon)*(*(outputFeatureMatrix + i*numOfFeatures + k)) + *(aggregationVar + i*numOfFeatures + k);
-		//}
-    //}
+	// index select
+	float *indexSelectOutput = (float*)calloc(numOfEdges*outputSize, sizeof(float));
+	indexSelectOutput = index_select(featureTensor, numOfNodes, outputSize, 0, edgeSources, numOfEdges, indexSelectOutput);
+	
+	// epsilon multiplication
+	// TODO: add weight constant to params
+	float *h_outputLinear = (float*)calloc(numOfNodes*outputSize, sizeof(float));
+	linear(indexSelectOutput, numOfNodes, outputSize,
+               h_outputLinear, numOfNodes, outputSize);
+	
+	return h_outputLinear;
+	
+	
+//if (thread_idx < numOfNodes*numOfFeatures) {
+//    
+//    // get indices of the thread
+//    
+//    //printf("thread_idx is: %d\n", thread_idx);
+//
+//    //printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d\n", blockIdx.x, blockDim.x, threadIdx.x);
+//
+//    const int64_t id_exEdges = (thread_idx % numOfDirectedEdges);
+//
+//    const int64_t id_exNodes = (thread_idx / numOfFeatures);
+//
+//    const int64_t id_exFeatures = (thread_idx / numOfNodes);
+//
+//    // if an incoming edge to respected node
+//    if( *(edgeIndex + numOfDirectedEdges + id_exEdges) == id_exNodes ) {
+//	// then apply aggregation scheme of GCN
+//	// to corresponding node's feature
+//	*(aggregationVar + (int)numOfFeatures*( (int)*(edgeIndex + numOfDirectedEdges + id_exEdges) )
+//	    + id_exFeatures) = *(featureTensor + thread_idx);
+//    }
+//	
+//    //sync threads before output update
+//    //__syncthreads();
+//
+//    // update output feature values
+//	*(outputFeatureMatrix + thread_idx) =
+////    *(outputFeatureMatrix + numOfFeatures*id_exNodes + id_exFeatures) =
+//	    (1 + epsilon)*(*(outputFeatureMatrix + thread_idx)) +
+//	    *(aggregationVar + thread_idx);
+//
+//    }
+//
+//    // the below part is on hold due to kernel update
+//    //if(i < numOfNodes) {
+//                //for(int j=0; j<numOfDirectedEdges; j++) {
+//			//if((*(edgeIndex + j)) == (float)i) { // if there is an edge incoming to node i
+//				//for(int k=0; k<numOfFeatures; k++) {
+//					//*(aggregationVar + i*numOfFeatures + k) += *(featureTensor + i*numOfFeatures + k);
+//				//}
+//			//}
+//                //}
+//
+//		//for(int k=0; k<numOfFeatures; k++) {
+//			//*(outputFeatureMatrix + i*numOfFeatures + k) = (1 + epsilon)*(*(outputFeatureMatrix + i*numOfFeatures + k)) + *(aggregationVar + i*numOfFeatures + k);
+//		//}
+//    //}
 
 
 }
