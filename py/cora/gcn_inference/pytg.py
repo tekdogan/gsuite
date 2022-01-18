6d51d9143b859ed04aab3f3a837ef5ef031502e3
tekdogan <tahatekdogan97@hotmail.com>
2022-01-19 01:07:35 +0300

    PyG GCN reconfigured as vanilla 1-layer

--- py/cora/gcn_inference/pytg.py
+++ py/cora/gcn_inference/pytg.py
@@ -19,17 +19,18 @@ with torch.autograd.profiler.emit_nvtx():
         def __init__(self):
             super(Net, self).__init__()
             self.conv1 = GCNConv(dataset.num_node_features, 16)
-            self.conv2 = GCNConv(16, dataset.num_classes)
+            #self.conv2 = GCNConv(16, dataset.num_classes)
 
         def forward(self, data):
             x, edge_index = data.x, data.edge_index
 
             x = self.conv1(x, edge_index)
-            x = F.relu(x)
+            #x = F.relu(x)
             #x = F.dropout(x, training=self.training)
-            x = self.conv2(x, edge_index)
+            #x = self.conv2(x, edge_index)
 
-            return F.log_softmax(x, dim=1)
+            #return F.log_softmax(x, dim=1)
+            return x
 
     device = torch.device('cuda')
     model = Net().to(device)
